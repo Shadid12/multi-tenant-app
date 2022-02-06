0a988fab55b2b98214a402b3258e3df6dba8dd43
shaque <shaque@omers.com>
2022-02-06 18:01:25 -0500

    ready to create items

---
+++ components/storeItem.js
@@ -0,0 +1,29 @@
+import Link from "next/link";
+import styles from '../styles/Stores.module.css';
+
+export default function StoreItem({ store }) {
+  console.log('store', store);
+  return (
+    <div className="tile is-child box">
+      <p className="title">{store.name}</p>
+      <p>
+        {store.category.join(', ')}
+        <div className={styles.buttonWrap}>
+          <Link href="/">
+            <a className="button">View Store Items</a>
+          </Link>
+        </div>
+      </p>
+      <div className={styles.buttonWrap}>
+        <Link href="/">
+          <a className="button is-success is-light">Add Items</a>
+        </Link>
+
+        <Link href="/">
+          <a className="button is-warning is-light">Edit Store</a>
+        </Link>
+        
+      </div>
+    </div>
+  )
+}
--- pages/shops/index.js
+++ pages/shops/index.js
@@ -1,29 +1,58 @@
 import Link from "next/link";
+import StoreItem from "../../components/storeItem";
+import { useUser } from "@auth0/nextjs-auth0";
+import { useQuery } from "urql";
+import { useEffect } from "react";
 import styles from '../../styles/Stores.module.css';
 
+export const GetMyShops = `
+  query GetMyShops($sub: String!){
+    userBySub(sub: $sub) {
+      _id
+      stores {
+        data {
+         _id
+         name
+         category
+        }
+      }
+    }
+  }
+`;
+
 export default function MyShops() {
+  const { user } = useUser();
+
+  useEffect(() => {
+    if(user?.sub) {
+      console.log('User found', user);
+      reexecuteQuery({ sub: user.sub });
+    }
+  }, [user]);
+
+  const [{fetching, data, error}, reexecuteQuery] = useQuery({
+    query: GetMyShops,
+    variables: { sub: user?.sub },
+  });
+
+  if(fetching) return <p>Loading...</p>;
+
+  console.log('data', data);
+
   return (
     <div className="container">
       <Link href="/shops/new">
         <a className="button is-success">Create a new shop</a>
       </Link>
-      <h1 className="title is-4">My Shops</h1>
-      <div className="tile is-ancestor">
-        <div className="tile is-4 is-vertical is-parent">
-          <div className="tile is-child box">
-            <p className="title">One</p>
-            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
-            <div className={styles.buttonWrap}>
-              <Link href="/">
-                <a className="button is-success is-light">Add Items</a>
-              </Link>
-
-              <Link href="/">
-                <a className="button is-warning is-light">Edit Store</a>
-              </Link>
+      <h1 className="title is-8">My Shops</h1>
+      <div className={styles.shopContainer}>
+        {
+          data?.userBySub?.stores?.data?.map(store => (
+            <div className="tile is-4 is-vertical is-parent" key={store._id}>
+              <StoreItem store={store}/>
             </div>
-          </div>
-        </div>
+          ))
+        }
       </div>
     </div>
   )
